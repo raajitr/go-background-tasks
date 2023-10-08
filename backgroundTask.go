e341b392b5abdf393f0e3c241dd0dd57ec8c666c
Raajit <raajit.raj@gmail.com>
2023-10-08 16:26:03 +0530

    Stats handler initial logic

--- backgroundTask.go
+++ backgroundTask.go
@@ -6,70 +6,82 @@ import (
 	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
-
 )
 
-type BackroundTask struct  {
+type Background struct {
 	isRunning chan bool
-	doneRow chan int
-	db *pgxpool.Pool
+	doneRow   chan int
+	db        *pgxpool.Pool
 }
 
-func NewBackgroundTask(isRunning chan bool, db *pgxpool.Pool) *BackroundTask{
-	return &BackroundTask{
-		isRunning: isRunning,
-		doneRow: make(chan int),
-		db: db,
+func NewBackgroundTask(db *pgxpool.Pool) *Background {
+	ret := &Background{
+		isRunning: make(chan bool, 1),
+		doneRow:   make(chan int, 1),
+		db:        db,
 	}
+
+	ret.isRunning <- false
+	ret.doneRow <- 0
+
+	return ret
 }
 
-func (b BackroundTask) toggleStatus(status bool) {
+func (b Background) toggleStatus(status bool) {
 	b.isRunning <- status
 }
 
-func (b BackroundTask) updateValues() error {
-    query := "SELECT id FROM test.Person FOR UPDATE SKIP LOCKED"
-    rows, err := b.db.Query(context.Background(), query)
+func (b Background) updateValues() error {
+	query := "SELECT id FROM test.Person FOR UPDATE SKIP LOCKED"
+	rows, err := b.db.Query(context.Background(), query)
 	fmt.Println(rows.Next(), "Line 1")
 
-    if err != nil {
-        fmt.Println(err, "Line 1")
+	if err != nil {
+		fmt.Println(err, "Line 1")
 		return err
-    }
+	}
 
-    for rows.Next() {
-        var id int
-        if err := rows.Scan(&id); err != nil {
-            fmt.Println(err, "Line 2")
+	for rows.Next() {
+		var id int
+		if err := rows.Scan(&id); err != nil {
+			fmt.Println(err, "Line 2")
 			return err
-        }
+		}
 
-        // Update the value for the current row
-        updateQuery := "UPDATE test.Person SET LastUpdated = $1 WHERE id = $2"
-        _, err := b.db.Exec(context.Background(), updateQuery, time.Now(), id)
-		b.doneRow <- id
-        if err != nil {
-            fmt.Println(err, "Line 3")
+		// Update the value for the current row
+		updateQuery := "UPDATE test.Person SET LastUpdated = $1 WHERE id = $2"
+		_, err := b.db.Exec(context.Background(), updateQuery, time.Now(), id)
+		if err != nil {
+			fmt.Println(err, "Line 3")
 			return err
-        }
-    }
+		}
+		fmt.Println(id)
+		
+		// don't really care if the message is being listened or not.
+		select {
+			case b.doneRow <- id:
+				continue; 
+			default:
+				continue;
+		}
+	}
 
-    if err := rows.Err(); err != nil {
-        fmt.Println(err, "Line 4")
+	if err := rows.Err(); err != nil {
+		fmt.Println(err, "Line 4")
 		return err
-    }
+	}
 
-    return nil
+	return nil
 }
 
-func (b BackroundTask) Start() {
+func (b Background) Start() {
 	b.isRunning <- true
 
 	// Simulate a long-running background job
 	fmt.Println("Background job started...")
 	b.updateValues()
 
-	defer func(){
+	defer func() {
 		fmt.Println("Background job completed.")
 		b.isRunning <- false
 	}()
--- main.go
+++ main.go
@@ -5,38 +5,41 @@ import (
 	"net/http"
 )
 
-var isRunning = make(chan bool, 1)
-
 func main() {
-	isRunning <- false
-	backgroundTask := NewBackgroundTask(isRunning)
+	db_conn := DBClient()
+	backgroundTask := NewBackgroundTask(db_conn)
 	fmt.Println("BG INITIATED")
-    // Define handler functions for the "start" and "stats" endpoints
-    http.HandleFunc("/start", startHandler(*backgroundTask))
-    http.HandleFunc("/stats", statsHandler)
+	// Define handler functions for the "start" and "stats" endpoints
+	http.HandleFunc("/start", startHandler(*backgroundTask))
+	http.HandleFunc("/stats", statsHandler(*backgroundTask))
 
-    // Start the HTTP server on port 8080
-    fmt.Println("Server is running on :8080")
-    err := http.ListenAndServe(":8080", nil)
-    if err != nil {
-        fmt.Println("Error starting server:", err)
-    }
+	// Start the HTTP server on port 8080
+	fmt.Println("Server is running on :8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
 }
 
-func startHandler(backgorundTask BackroundTask) http.HandlerFunc{
+func startHandler(b Background) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 
-		if <-isRunning {
+		if <-b.isRunning {
 			fmt.Fprintln(w, "It's already running")
 			return
 		}
 		// Handle the "start" endpoint
 		fmt.Fprintln(w, "Starting the job")
 
-		go backgorundTask.Start()
+		go b.Start()
 	}
 }
-func statsHandler(w http.ResponseWriter, r *http.Request) {
-    // Handle the "stats" endpoint
-    fmt.Fprintln(w, "This is the 'stats' endpoint!")
+func statsHandler(b Background) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Handle the "stats" endpoint
+		currentRows := <-b.doneRow
+		msg := fmt.Sprintf("Current Completed Rows: %d", currentRows)
+
+		fmt.Fprintln(w, msg)
+	}
 }
